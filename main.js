828c628cd0f02114151956fe42138c47589c39f2
xlk <1148038404@qq.com>
2018-06-08 23:18:36 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var allButtons = $('#buttons > li')
+
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function(x) {
+    var index = $(x.currentTarget).index()
+    var p = index * -920
+    $('#images').css({
+      transform: 'translate(' + p + 'px)'
+    })
+    n = index
+    
+  })
+}
+
+
+
+var n = 0;
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click')
+
+
+var timerId = setTimer()
+
+function setTimer() {
+  return setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click')
+  }, 3000)
+}
+
+
+
+$('.window').on('mouseenter', function() {
+  window.clearInterval(timerId)
+})
+
+$('.window').on('mouseleave', function() {
+  timerId = setTimer()
+})
